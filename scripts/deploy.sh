b1008bc49591306b52712c12ba3909096bb10c14
Pascal Brandt <psbrandt@gmail.com>
2018-04-02 15:30:19 -0700

    🍁 Make it work.

--- scripts/deploy.sh
+++
@@ -1,3 +0,0 @@
-# Deploy to CIRG IHE VM
-echo "Deploying CIRG IHE VM"
-sshpass -p$TRAVIS_SSH_PASS scp -o StrictHostKeyChecking=no -r dist/* travis@ihe.cirg.washington.edu:/var/www/himss/healthy-weight/
